0b0710c0b31a47fdfa80ba8e6a0ec2c88407a3c4
Edgardo Rafael Yoliani Cantillo <edrayoca@outlook.es>
2021-04-28 10:23:06 -0500

    Obtener peliculas populares y Horizontal PageView

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:peliculas/peliculas_provider.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+import 'package:peliculas/src/widgets/movie_horizontal.dart';
 
 class HomePage extends StatelessWidget {
   final peliculasProvider = new PeliculasProvider();
@@ -8,7 +9,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Peliculas'),
+        title: Text('Películas en cines'),
         backgroundColor: Colors.indigoAccent,
         actions: <Widget>[
           IconButton(icon: Icon(Icons.search), onPressed: () {})
@@ -16,8 +17,10 @@ class HomePage extends StatelessWidget {
       ),
       body: Container(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             _swiperTarjetas(),
+            _footer(context),
           ],
         ),
       ),
@@ -38,6 +41,41 @@ class HomePage extends StatelessWidget {
               ));
         }
       },
+    );
+  }
+
+  Widget _footer(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            child: Text(
+              'Populares',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            padding: EdgeInsets.only(left: 20.0),
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          FutureBuilder(
+            future: peliculasProvider.getPopulares(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                return MovieHorizontal(peliculas: snapshot.data);
+              } else {
+                return Container(
+                    height: 400,
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ));
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
 }
